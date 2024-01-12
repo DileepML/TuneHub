907a9e0e87a4c611b69e52cf44309f73ce1f47da
dilee <dilee@DileepGowda>
2024-01-12 16:06:06 +0530

    tunehub

---
+++ src/main/java/com/example/demo/Tunehub1Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tunehub1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tunehub1Application.class, args);
+	}
+
+}
+ 
